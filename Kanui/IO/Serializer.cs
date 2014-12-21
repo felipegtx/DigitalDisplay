b0553bca789056e888b1313fdcaa0cc39bbcc93d
Felipe Teixeira <felipe.g.te@gmail.com>
2014-12-21 13:34:38 -0200

    Versão base da solução para o desafio 'Digital display'
    Fazendo uso de uma função de hash, a solução desenvolvida classifica
    cada um dos possíveis dígitos de forma a identificá-los usando uma
    complexidade próxima a O(1)

---
+++ Kanui/IO/Serializer.cs
@@ -0,0 +1,28 @@
+﻿using Kanui.IO.Abstractions;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace Kanui.IO
+{
+    internal sealed class Serializer : ISerializer
+    {
+        public void Serialize<SomeType>(SomeType data, string pathToFile)
+        {
+            var formatter = new BinaryFormatter();
+            using (var stream = new FileStream(pathToFile, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                formatter.Serialize(stream, data);
+            }
+        }
+
+        public SomeType DeserializeFrom<SomeType>(string pathToFile)
+        {
+            var formatter = new BinaryFormatter();
+            using (var stream = new FileStream(pathToFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return (SomeType)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
